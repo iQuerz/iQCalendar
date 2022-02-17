cd4481df73305b09c3cace9ff01fd206e613e189
iQuerz <niki.tenis@gmail.com>
2022-02-17 02:04:48 +0100

    Add settings controller

---
+++ ServerAPI/Business/SettingsBusiness.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using ServerAPI.Data.Models;
+
+namespace ServerAPI.Business
+{
+    public partial class Logic
+    {
+        public async Task<Settings> GetSettings(string name)
+        {
+            var settings = _context.Settings.Where(s => s.ServerName == name);
+
+            if(settings == null)
+            {
+                var err = new iQError
+                {
+                    Error = "Invalid server name.",
+                    Details = "S02 - Wrong server name. Cannot access settings without it."
+                };
+                throw new iQException(err, 400);
+            }
+
+            return settings.FirstOrDefault();
+        }
+        public async Task<Settings> UpdateSettings(Settings settings)
+        {
+            var s = await _context.Settings.FindAsync(settings.revisionID);
+
+            if(s == null)
+            {
+                var err = new iQError
+                {
+                    Error = "Settings not found.",
+                    Details = "S03 - Specified settings do not exist."
+                };
+                throw new iQException(err, 404);
+            }
+
+            s.HostEmailPassword = settings.HostEmailPassword;
+            s.HostEmailUsername = settings.HostEmailUsername;
+            s.NotificationTime = settings.NotificationTime;
+            s.ServerName = settings.ServerName;
+            s.Port = settings.Port;
+            s.revisionDate = System.DateTime.Now;
+
+            return s;
+        }
+    }
+}
---
+++ ServerAPI/Controllers/SettingsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+using ServerAPI.Business;
+using ServerAPI.Data;
+using ServerAPI.Data.Models;
+
+namespace ServerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SettingsController : Controller
+    {
+        Logic _logic;
+        public SettingsController(CalendarContext context)
+        {
+            _logic = new Logic(context);
+        }
+
+        [HttpGet]
+        [Route("name")]
+        public async Task<ActionResult> getSettings(string name)
+        {
+            try
+            {
+                return Ok(await _logic.GetSettings(name));
+            }
+            catch (iQException e)
+            {
+                return StatusCode(e.StatusCode, e.Error);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> updateSettings([FromBody] Settings settings)
+        {
+            try
+            {
+                return Ok(await _logic.UpdateSettings(settings));
+            }
+            catch (iQException e)
+            {
+                return StatusCode(e.StatusCode, e.Error);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
