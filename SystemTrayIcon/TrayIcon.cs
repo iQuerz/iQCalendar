0b77aa166ea93b369d0e3846566c39d54e3532f8
iQuerz <niki.tenis@gmail.com>
2022-02-17 15:50:48 +0100

    Add system tray icon

---
+++ SystemTrayIcon/TrayIcon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemTrayIcon
+{
+    public class TrayIcon
+    {
+        NotifyIcon _trayIcon;
+        public TrayIcon(bool start)
+        {
+            _trayIcon = new NotifyIcon();
+
+            if (start)
+                Start();
+        }
+
+        public void Start()
+        {
+            _trayIcon.Visible = true;
+            _trayIcon.Icon = Icon.ExtractAssociatedIcon("Resources/icon.ico");
+        }
+    }
+}
